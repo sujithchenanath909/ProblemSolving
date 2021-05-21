69156d5e7a0e35e474112383667383fded92d45c
Sujith Chenanath <sujithchenanath@gmail.com>
2021-05-21 18:52:50 +0530

    value

--- src/samples/RearrangeArray.java
+++ src/samples/RearrangeArray.java
@@ -1,6 +1,7 @@
 package samples;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
  * Rearrange a given array so that Arr[i] becomes Arr[Arr[i]] with O(1) extra space.
@@ -14,7 +15,7 @@ Return : [0, 1]
  * */
 public class RearrangeArray {
 
-	 public void arrange(ArrayList<Integer> a) {
+	 public static void arrange(ArrayList<Integer> a) {
 	    int n=a.size();
 		  for (int i = 0; i < n; i++)
 			  a.set(i, a.get(i)+(a.get(a.get(i))%n)*n);
@@ -24,12 +25,38 @@ public class RearrangeArray {
 	        for (int i = 0; i < n; i++)
 	        	a.set(i, a.get(i)/n);
 	         
-	 
+	        System.out.println(a.toString());
 	 }
+	 
+	 //another solution from geeks not tested!!
+	 void rearrangeNaive(int arr[], int n)
+	    {
+	        // Create an auxiliary array of same size
+	        int temp[] = new int[n];
+	        int i;
+	 
+	        // Store result in temp[]
+	        for (i = 0; i < n; i++)
+	            temp[arr[i]] = i;
+	 
+	        // Copy temp back to arr[]
+	        for (i = 0; i < n; i++)
+	            arr[i] = temp[i];
+	    }
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ArrayList<Integer> list=new ArrayList<Integer>();
+		list.add(1);
+		list.add(3);
+		list.add(0);
+		list.add(2);
+		System.out.println(list.toString());
+		arrange(list);
+		
+		System.out.println(list.toString());
 
 	}
 
