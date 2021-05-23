db471e04204773bf342700714a08eeb6878e8656
Sujith Chenanath <sujithchenanath@gmail.com>
2021-05-23 18:59:30 +0530

    reverse

---
+++ src/samples/ReverseInteger.java
@@ -0,0 +1,31 @@
+package samples;
+
+public class ReverseInteger {
+	
+	 public static int reverse(int A) {
+         boolean negative = false;
+        if (A < 0) {
+            negative = true;
+            A *= -1;
+        }
+        
+        int rev = 0;
+        int prevRev = 0;
+        while (A > 0) {
+            int curr = A%10;
+            rev = rev*10 + curr;
+            if ((rev - curr)/10 != prevRev) return 0;
+            prevRev = rev;
+            A /= 10;
+        }
+        
+        return negative ? -1*rev : rev;
+    }
+    
+	 
+	 public static void main(String[] args) {
+		 
+		 System.out.println(reverse(123));
+	 }
+
+}
