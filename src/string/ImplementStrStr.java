b412b64a2f5520630c6a80fa8639a0e8dc548179
Sujith Chenanath <sujithchenanath@gmail.com>
2021-05-31 21:30:17 +0530

    sol

--- src/string/ImplementStrStr.java
+++ src/string/ImplementStrStr.java
@@ -10,7 +10,7 @@ Returns the index of the first occurrence of needle in haystack, or -1 if needle
  * */
 public class ImplementStrStr {
 	
-	 public int strStr(final String A, final String B) {
+	 public static int strStr(final String A, final String B) {
 		    int lenA = A.length();
 		    int lenB = B.length();
 		    if (lenB > lenA) {
@@ -23,8 +23,39 @@ public class ImplementStrStr {
 		    }
 		    return -1;
 		  }
+	 
+	 //with out using subString method
+	 public static int strStr2(final String A, final String B) {
+		 
+		 if(B.isEmpty())
+				 return 0;
+		 
+		 int m=A.length();
+		 int n=B.length();
+		 
+		 if(m<n)
+			 return -1;
+		 
+		 for(int i=0;i<=m-n;++i) {
+			 int j;
+			 for( j=0;j<n;++j)
+			 {
+				 
+				 if(A.charAt(i+j) != B.charAt(j))
+					 break;
+			 }
+			 if(j==n)
+				 return i;
+			 
+		 }
+
+		 return -1;
+	 }
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println(strStr("hello", "lo"));
+		System.out.print(strStr2("hello", "ll"));
 
 	}
 
