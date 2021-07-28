8fdeeed79320e77e06085c78919c74d419e6f086
sujithchenanath909 <sujithchenanath@gmail.com>
2021-07-28 18:09:00 +0530

    ll

--- src/linkedlists/ListNode.java
+++ src/linkedlists/ListNode.java
@@ -3,37 +3,36 @@ package linkedlists;
 public class ListNode<E> {
 
 	Node<E> head;
-		
+
 	void add(E data) {
-		
-		Node<E> toAdd=new Node<E>(data);
-		if(isEmpty()) {
-			head=toAdd;
+
+		Node<E> toAdd = new Node<E>(data);
+		if (isEmpty()) {
+			head = toAdd;
 			return;
 		}
-		Node<E> temp=head;
-		while(temp.next != null) {
-			
-			temp=temp.next;
-		} 
-		temp.next=toAdd;
-		
+		Node<E> temp = head;
+		while (temp.next != null) {
+
+			temp = temp.next;
+		}
+		temp.next = toAdd;
+
 	}
 
-	
 	public boolean isEmpty() {
-		
-		return head==null;
-		
+
+		return head == null;
+
 	}
-	
-	static class  Node<E>{
+
+	static class Node<E> {
 		E value;
 		Node<E> next;
-		
-		public Node(E data){
-			this.value=data;
-			next=null;
+
+		public Node(E data) {
+			this.value = data;
+			next = null;
 		}
 	}
 }
